81c59991e8009a403e2ed8e26f2c63ef4b65101e
Seonghun Lim <wariua@gmail.com>
2021-08-16 12:30:27 +0900

    Hide menu bar in electron app

--- src/background.js
+++ src/background.js
@@ -24,6 +24,9 @@ async function createWindow () {
     }
   })
 
+  // Hide menu bar
+  win.removeMenu()
+
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
