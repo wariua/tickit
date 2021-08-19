bb8e8bec9e06407440005b706548abe8d7445052
Seonghun Lim <wariua@gmail.com>
2021-08-19 21:38:23 +0900

    Introduce two pane structure
    The resizable left pane will contain three lists of events. The right
    pane will display pages for selected events.
    
    It is implemented with vue-multipane, and it seems not to work smoothly
    in electron apps...

--- src/App.vue
+++ src/App.vue
@@ -32,28 +32,88 @@
       </v-btn>
     </v-app-bar>
 
-    <v-main>
+    <v-main id="main-container">
+      <multipane class="virtical-panes" layout="vertical">
+        <div class="pane" :style="{ minWidth: '200px', width: '300px' }">
+          <event-lists
+            v-bind:src="eventSource"
+          />
+        </div>
+        <multipane-resizer/>
+        <div class="pane" :style="{ flexGrow: 1 }">
+          <event-detail
+            v-bind:href="eventUrl"
+          />
+        </div>
+      </multipane>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import { Multipane, MultipaneResizer } from 'vue-multipane'
+import EventLists from './components/EventLists'
+import EventDetail from './components/EventDetail'
+
 export default {
   name: 'App',
 
   components: {
+    Multipane,
+    MultipaneResizer,
+    EventLists,
+    EventDetail
   },
 
   data: () => ({
     eventSource: null,
+    eventUrl: null,
 
     eventSources: []
   })
 }
 </script>
 
-<style>
+<style lang="scss">
 html {
   overflow-y: auto !important;
+}
+
+#main-container {
+  height: 100vh;
+}
+
+.virtical-panes {
+  width: 100%;
+  height: 100%;
+}
+
+.virtical-panes > .multipane-resizer {
+  margin: 0; left: 0;
+  position: relative;
+
+  width: 7px;
+
+  &:before {
+    display: block;
+    content: "";
+    width: 3px;
+    height: 20px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -10px;
+    margin-left: -1.5px;
+    border-left: 1px solid transparent;
+    border-right: 1px solid transparent;
+  }
+
+  &:hover {
+    background-color: #ddd;
+
+    &:before {
+      border-color: #999;
+    }
+  }
 }
 </style>
---
+++ src/components/EventLists.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <span>Event list from {{ src }}</span>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'EventLists',
+
+  props: ['src'],
+
+  watch: {
+    src: (newValue) => {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
