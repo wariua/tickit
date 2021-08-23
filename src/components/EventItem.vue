d1a57d432586b36d4b0504ae3c560e1984247115
Seonghun Lim <wariua@gmail.com>
2021-08-23 22:56:32 +0900

    Implement event lists and items
    It is still static and stateless.

---
+++ src/components/EventItem.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-hover>
+    <template v-slot:default="{ hover }">
+      <v-card
+        v-bind:id="'item-' + item.id"
+        outlined
+        class="event-item ma-1"
+        v-bind:class="item.focused ? 'event-item-focused' : ''"
+        v-bind:ripple="false"
+        tabindex="-1"
+      >
+        <v-list-item class="pa-0 ma-1">
+          <v-list-item-content class="pa-1">
+            <v-list-item-title class="font-weight-bold">
+              {{ item.title }}
+            </v-list-item-title>
+
+            <v-layout>
+              <v-flex xs2>
+                <v-img
+                  v-bind:src="item.imgUrl"
+                  height=60
+                  contain
+                />
+              </v-flex>
+
+              <v-flex xs10 class="pa-1">
+                <div class="description">
+                  {{ item.dateFrom + '~' + item.dateTo }}<br>
+                  {{ item.place }}
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-fade-transition>
+          <v-overlay
+            v-if="hover"
+            absolute
+            color="#aaa"
+            opacity=0.3
+          >
+            <v-btn
+              v-if="toLeft"
+              icon
+              x-large
+              color="black"
+              justify="center"
+            >
+              <v-icon>mdi-arrow-left</v-icon>
+            </v-btn>
+            <v-icon
+              v-if="!toLeft"
+              x-large
+              color="transparent"
+            >
+              mdi-arrow-left
+            </v-icon>
+
+            <v-btn
+              icon
+              x-large
+              class="mx-6"
+              color="black"
+            >
+              <v-icon>mdi-open-in-new</v-icon>
+            </v-btn>
+
+            <v-btn
+              v-if="toRight"
+              icon
+              x-large
+              color="black"
+            >
+              <v-icon>mdi-arrow-right</v-icon>
+            </v-btn>
+            <v-icon
+              v-if="!toRight"
+              x-large
+              color="transparent"
+            >
+              mdi-arrow-right
+            </v-icon>
+          </v-overlay>
+        </v-fade-transition>
+      </v-card>
+    </template>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  name: 'EventItem',
+
+  props: {
+    item: Object,
+    toLeft: Boolean,
+    toRight: Boolean
+  },
+
+  watch: {
+    focus: function (newValue) {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/components/EventLists.vue
+++ src/components/EventLists.vue
@@ -1,48 +1,204 @@
 <template>
-  <v-container>
-    <span>Event list from {{ src }}</span>
-  </v-container>
+  <v-card
+    height="100%"
+    flat
+  >
+    <v-tabs
+      v-model="tab"
+      grow
+      height=10
+      slider-size=8
+    >
+      <v-tabs-slider color="blue-grey lighten-3"></v-tabs-slider>
+
+      <v-tab
+        v-for="list in lists"
+        v-bind:key="list"
+        v-bind:href="`#tab-${list}`"
+      >
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab" style="height: calc(100% - 10px)">
+      <v-tab-item
+        v-for="list in lists"
+        v-bind:key="list"
+        v-bind:value="'tab-' + list"
+        style="height: calc(100% + 10px)"
+      >
+        <v-list max-height="100%" class="overflow-y-auto py-0">
+          <event-item
+            v-for="ev in events[list]"
+            v-bind:key="ev.id"
+            v-bind:id="'event-' + ev.id"
+            v-bind:item="ev"
+            v-bind:to-left="list != 'left'"
+            v-bind:to-right="list != 'right'"
+          />
+        </v-list>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
 </template>
 
 <script>
+import isElectron from 'is-electron'
+import EventItem from './EventItem'
+
 export default {
   name: 'EventLists',
+
+  components: {
+    EventItem
+  },
 
   props: ['src'],
 
   data: function () {
     return {
+      lists: ['left', 'center', 'right'],
+
+      allEvents: {
+        left: [],
+        center: [],
+        right: []
+      },
+
+      tab: 'tab-center',
+
       eventSources: [
         {
           id: 'interpark-musical',
-          text: '인터파크 뮤지컬'
+          text: '인터파크 뮤지컬',
+          url: 'http://ticket.interpark.com/TPGoodsList.asp?Ca=Mus&Sort=5',
+          parse: this.parseInterpark,
+          eventUrl: this.eventUrlInterpark
         },
         {
           id: 'interpark-concert',
-          text: '인터파크 콘서트'
+          text: '인터파크 콘서트',
+          url: 'http://ticket.interpark.com/TPGoodsList.asp?Ca=Liv&Sort=5',
+          parse: this.parseInterpark,
+          eventUrl: this.eventUrlInterpark
         },
         {
           id: 'interpark-classic',
-          text: '인터파크 클래식/무용'
+          text: '인터파크 클래식/무용',
+          url: 'http://ticket.interpark.com/TPGoodsList.asp?Ca=Cla&Sort=5',
+          parse: this.parseInterpark,
+          eventUrl: this.eventUrlInterpark
         },
         {
           id: 'interpark-drama',
-          text: '인터파크 연극'
+          text: '인터파크 연극',
+          url: 'http://ticket.interpark.com/TPGoodsList.asp?Ca=Dra&Sort=5',
+          parse: this.parseInterpark,
+          eventUrl: this.eventUrlInterpark
         },
         {
           id: 'interpark-les',
-          text: '인터파크 전시/행사'
+          text: '인터파크 전시/행사',
+          url: 'http://ticket.interpark.com/TPGoodsList.asp?Ca=Eve&SubCa=Eve_T&Sort=5',
+          parse: this.parseInterpark,
+          eventUrl: this.eventUrlInterpark
         },
         {
           id: 'interpark-family',
-          text: '인터파크 아동/가족'
+          text: '인터파크 아동/가족',
+          url: 'http://ticket.interpark.com/TPGoodsList.asp?Ca=Fam&Sort=5',
+          parse: this.parseInterpark,
+          eventUrl: this.eventUrlInterpark
         }
       ]
     }
   },
 
+  computed: {
+    events: function () {
+      return {
+        left: this.allEvents.left,
+        center: this.allEvents.center,
+        right: this.allEvents.right
+      }
+    }
+  },
+
   watch: {
-    src: (newValue) => {
+    src: function (newValue) {
+      this.fetchEvents(newValue)
+    }
+  },
+
+  methods: {
+    fetchEvents (src) {
+      const eventSource = this.eventSources.find(x => x.id === src)
+
+      /* NOTE: Browsers will return emptry responses by CORS policy.
+       * Here are two workarounds:
+       *
+       * - Run in electron with webSecurity=false, which disables CORS
+       *   check.
+       * - Enable the proxy in the dev server that relays unknown requests
+       *   to a predefined server.
+       */
+      var url = eventSource.url
+      if (!isElectron()) {
+        const parsed = new URL(url)
+        url = parsed.pathname + parsed.search
+      }
+
+      fetch(url, {
+        credentials: 'omit',
+        referrerPolicy: 'no-referrer'
+      })
+        .then(response => response.arrayBuffer())
+        .then(bytes => eventSource.parse(bytes))
+        .then(events => this.updateLists(events))
+    },
+
+    parseInterpark (bytes) {
+      const decoder = new TextDecoder('euc-kr')
+      const parser = new DOMParser()
+      const doc = parser.parseFromString(decoder.decode(bytes), 'text/html')
+      const rows = doc.querySelectorAll('div.stit tr')
+
+      var events = []
+
+      for (const row of rows) {
+        const aThumb = row.querySelector('td.RKthumb a')
+        if (aThumb === null) { // table header
+          continue
+        }
+        const id = aThumb.href.split('=').pop()
+        const imgUrl = aThumb.querySelector('img').src
+
+        const aTitle = row.querySelector('td.RKtxt a')
+        const title = aTitle.text
+
+        const tdPlaceAndDate = row.querySelectorAll('td.Rkdate')
+        const place = tdPlaceAndDate[0].querySelector('a').text
+        const datePair = tdPlaceAndDate[1].innerText
+          .replace(/\s+/g, '').split('~')
+
+        events.push({
+          id,
+          title,
+          imgUrl,
+          place,
+          dateFrom: datePair[0],
+          dateTo: datePair[1]
+        })
+      }
+
+      return events
+    },
+
+    updateLists (events) {
+      this.allEvents.center = events
+    },
+
+    eventUrlInterpark (id) {
+      return 'http://ticket.interpark.com/Ticket/Goods/GoodsInfo.asp?GroupCode=' + id
     }
   }
 }
