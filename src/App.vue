516fd023c82d8257f46e3267643ef19db99d30d1
Seonghun Lim <wariua@gmail.com>
2021-09-02 19:03:24 +0900

    Delete bundled HelloWorld component
    Other bundled files are also removed.

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,15 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
 
   components: {
-    HelloWorld
   },
 
   data: () => ({
